e154522c294e6e8b5f63263eb8ed8cd24d6fdd9e
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-06 02:27:56 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
 
   def new
   end
+ 
+  def create
+     @user = User.new(user_params)
+ 
+    if @user.valid?
+      @user.save  # バリデーションをクリアした時
+      return redirect_to root_path
+    else
+      render "new"    # バリデーションに弾かれた時
+    end
+  end
 
   protected
 
