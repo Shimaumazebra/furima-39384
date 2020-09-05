a637a23036d23592a52b9ea5ea15e889680fc1c9
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-05 20:06:19 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          validates :name, presence: true
-         validates :last_name, presence: true
+         validates :last_name , presence: true
          validates :first_name, presence: true
       
          has_many :items
