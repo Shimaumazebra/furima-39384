d53db98b1ba75555a28a618d4f9717138e3b58d4
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-07 22:15:13 +0900

    バリデーションの設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
          has_many :perchases
          
          validates :name          , presence: true
-         validates :date_of_birth , presence: true
-         validates :email         , presence: true
          with_options presence: true do
-         validates :password              , format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+         validates :email                 , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
+         validates :password              , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
+         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
          validates :first_name            , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
          validates :last_name             , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-         validates :first_name_kana       , format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-         validates :last_name_kana        , format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :first_name_kana       , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
+         validates :last_name_kana        , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
+         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
          end
 
 
