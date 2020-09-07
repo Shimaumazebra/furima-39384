34dc1a2adb873a081252d3bfdc74821e8b30e4ce
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-07 22:18:25 +0900

    バリデーションの設定＃３

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
          validates :last_name             , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
          validates :first_name_kana       , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
          validates :last_name_kana        , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
-         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
+         validates :date_of_birth         , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
          end
 
 
