7625ffe064158b7cf896b36197f097f12e6e5627
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-08 12:15:45 +0900

    インデント設定#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,12 @@ class User < ApplicationRecord
          validates :name 
          validates :email 
          validates :date_of_birth
-         validates :password              , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
-         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i,    message: "is invalid. Input half-width characters."}
-         validates :first_name            , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-         validates :last_name             , format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-         validates :first_name_kana       , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
-         validates :last_name_kana        , format: { with: /\A[ァ-ヶー－]+\z/,   message: "is invalid. Input full-width katakana characters."}
+         validates :password              , format: { with: /\A[a-z0-9]+\z/i    , message: "is invalid. Input half-width characters."}
+         validates :password_confirmation , format: { with: /\A[a-z0-9]+\z/i    , message: "is invalid. Input half-width characters."}
+         validates :first_name            , format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: "is invalid. Input full-width characters."}
+         validates :last_name             , format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: "is invalid. Input full-width characters."}
+         validates :first_name_kana       , format: { with: /\A[ァ-ヶー－]+\z/   , message: "is invalid. Input full-width katakana characters."}
+         validates :last_name_kana        , format: { with: /\A[ァ-ヶー－]+\z/   , message: "is invalid. Input full-width katakana characters."}
          end
 
 
