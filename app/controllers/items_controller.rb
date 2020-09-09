596e10916484e8d462c800e5e5301227ac6450b4
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-09 18:55:05 +0900

    出品画像の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,14 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
   end
+  
   def new
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :estimated
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+
 
   belong_to :users
   has_one   :perchases
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 } 
