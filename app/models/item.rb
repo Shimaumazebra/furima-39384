c1e0f0f7afcea98559c3142cbbe2b67f0c7089ae
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-09 17:05:28 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 
   belong_to :users
   has_one   :perchases
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
 
 end
