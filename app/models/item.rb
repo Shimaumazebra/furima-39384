903dbdca7c4a90342fd9af4bf416bc26f8281925
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-13 01:03:31 +0900

    バリデーションの設定#1

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,26 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :prefecture
 
-
   belongs_to :user
-  has_one   :perchases
+  has_one    :perchase
   has_one_attached :image
 
+  with_options presence: true do
 
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :prefecture
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+    validates :estimated_shipping_id
+    validates :postage_fee_id
+  end
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :estimated_shipping_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :postage_fee_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
+
