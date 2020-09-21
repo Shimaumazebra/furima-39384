fefd8b6e1b78a43177088be9ef3abeb8b9a40085
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-21 21:04:11 +0900

    バリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,18 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :address
+
+  with_options presence: true do
+    validates :card_num
+    validates :cvc
+    validates :exp_month
+    validates :exp_year
+    validates :post_code
+    validates :address
+    validates :city
+    validates :phone_num
+    end
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_num, format: {/^(0{1}\d{9,10})$/, message: "is invalid. Without hyphen(-)"}
 end
