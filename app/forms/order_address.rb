223ff7c78c75a99ed3ba23c0ef8a2f296be582f4
Shimaumazebra <69620875+Shimaumazebra@users.noreply.github.com>
2020-09-22 11:33:44 +0900

    バリデーションの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,6 +2,16 @@ class OrderAddress
 
   include ActiveModel::Model
   attr_accessor :name, :user_id, :token, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_num
+  
+  with_options presence: true do
+    validates :token
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :address
+    validates :city
+    validates :phone_num, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+    end
+ 
   def save
     # 注文の情報を保存
     order = Order.create(user_id: user_id, item_id: item_id)
